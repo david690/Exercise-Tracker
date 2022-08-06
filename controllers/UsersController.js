81cb654475d95b7105a490b688dec83e191cd8ae
david690 <ricardodavid690@yahoo.com>
2022-08-06 16:15:57 -0500

    v1.0.0

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -28,7 +28,6 @@ exports.addUser = (req, res) => {
 
 exports.addExercise = (req, res) => {
   let newExercise = null;
-  console.log("date:", new Date(req.body.date))
   if (req.body.date) {
     newExercise = new Exercise({
       description: req.body.description,
@@ -60,15 +59,16 @@ exports.addExercise = (req, res) => {
 }
 
 exports.getExerciseLogs = (req, res) => {
-  User.findById(req.params._id).then(userData => {
-    Exercise.countDocuments({ userid: req.params._id }).then(logCount => {
-      Exercise.find({ userid: req.params._id }).then(exerciseData => {
+  if (req.query.from && req.query.to && req.query.limit) {
+    console.log("-------------------")
+    User.findById(req.params._id).then(userData => {
+      Exercise.find({ userid: req.params._id, date: { $gte: new Date(req.query.from), $lte: new Date(req.query.to) } }, {}, { limit: req.query.limit }).then(exerciseData => {
         let results = [];
 
         for (let value in exerciseData) {
-          results.push({ description: exerciseData[value].description, duration: exerciseData[value].duration, date: exerciseData[value].date });
+          results.push({ description: exerciseData[value].description, duration: exerciseData[value].duration, date: exerciseData[value].date.toDateString() });
         }
-        res.json({ _id: userData._id, username: userData.username, count: logCount, log: results });
+        res.json({ _id: userData._id, username: userData.username, count: parseInt(req.query.limit), log: results });
       }).catch(error => {
         console.log("error:", error);
         res.json({ error });
@@ -77,8 +77,27 @@ exports.getExerciseLogs = (req, res) => {
       console.log("error:", error);
       res.json({ error });
     });
-  }).catch(error => {
-    console.log("error:", error);
-    res.json({ error });
-  });
+  } else {
+    User.findById(req.params._id).then(userData => {
+      Exercise.countDocuments({ userid: req.params._id }).then(logCount => {
+        Exercise.find({ userid: req.params._id }).then(exerciseData => {
+          let results = [];
+
+          for (let value in exerciseData) {
+            results.push({ description: exerciseData[value].description, duration: exerciseData[value].duration, date: exerciseData[value].date.toDateString() });
+          }
+          res.json({ _id: userData._id, username: userData.username, count: logCount, log: results });
+        }).catch(error => {
+          console.log("error:", error);
+          res.json({ error });
+        });
+      }).catch(error => {
+        console.log("error:", error);
+        res.json({ error });
+      });
+    }).catch(error => {
+      console.log("error:", error);
+      res.json({ error });
+    });
+  }
 }
